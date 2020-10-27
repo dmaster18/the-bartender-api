fc5364fab1aa007114f739c1621347e3a2559a89
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-26 21:28:18 -0400

    Commit #11

--- app/models/ingedient.rb
+++ app/models/ingedient.rb
@@ -1,2 +1,8 @@
 class Ingedient < ApplicationRecord
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,4 @@
 class Question < ApplicationRecord
+  validates :name, :answer, presence: true
+  validates :name, :answer, uniqueness: true
 end

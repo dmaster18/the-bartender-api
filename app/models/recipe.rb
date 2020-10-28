96fda98bff6ece7e66321f84f8ffbc2ac2ed9e41
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-27 22:52:09 -0400

    Commit #39

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,14 @@
 class Recipe < ApplicationRecord
-  has_many :recipe_ingredients
-  has_many :ingredients, through: :recipe_ingredients
+  #has_many :recipe_ingredients
+  #has_many :ingredients, through: :recipe_ingredients
+  has_many :recipe_liquors
+  has_many :liquors, through: :recipe_liquors
+  has_many :recipe_liqueurs
+  has_many :liqueurs, through: :recipe_liqueurs
+  has_many :recipe_mixers
+  has_many :mixers, through: :recipe_mixers
+  has_many :recipe_garnishes
+  has_many :recipe_garnishes, through: :recipe_garnishes
   has_many :user_recipes
   has_many :users, through: :user_recipes
 

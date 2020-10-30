aed02e670018bfd7ce6572d24c4b7eabf45aea06
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-29 21:42:21 -0400

    Commit #94

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,13 @@
 class RecipesController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: RecipeSerializer.new(recipes).to_serialized_json
+  end
+
+  def show
+    recipe = Recipe.find_by(id: params[:id])
+    render json: RecipeSerializer.new(recipe).to_serialized_json
+  end
+
+
 end

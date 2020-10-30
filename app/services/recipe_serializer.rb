0aab9a63ce110337c93ca84a24b66ef84b1e21fa
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-29 21:56:50 -0400

    Commit #96

--- app/services/recipe_serializer.rb
+++ app/services/recipe_serializer.rb
@@ -1,17 +1,19 @@
 class RecipeSerializer
+  include FastJsonapi::ObjectSerializer
+  belongs_to :user
+  has_many :liquors, :liqueurs, :mixers, :garnishes
+#  def initialize(recipe)
+#    @recipe = recipe
+#  end
 
-  def initialize(recipe)
-    @recipe = recipe
-  end
-
-  def to_serialized_json
-    options = {include:
-      {
+  #def to_serialized_json
+  #  options = {include:
+  #    {
       #:liquors, :liqueurs, :mixers, :garnishes
-      }
-    }
-    @recipe.to_json(options)
-  end
+  #    }
+#    }
+#    @recipe.to_json(options)
+#3  end
 
 
 end

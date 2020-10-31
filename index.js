d6c05f52b58fbc58490ef4723cc0692dbea8ca63
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-30 20:48:42 -0400

    Commit #107

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+function fetchRecipes() {
+  recipes_url = "http://localhost:3000/recipes"
+  return fetch(recipes_url)
+  .then(resp => resp.json())
+  .then(json => renderRecipes(json))
+}
+
+function renderBooks(json) {
+  const main = document.querySelector('main')
+  json.forEach(book => {
+    const h2 = document.createElement('h2')
+    h2.innerHTML = `<h2>${book.name}</h2>`
+    main.appendChild(h2)
+  })
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+})

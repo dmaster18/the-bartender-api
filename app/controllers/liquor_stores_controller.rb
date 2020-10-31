ecd020753943f1e200ecff25183dc89d577743fe
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-30 21:28:22 -0400

    Commit #110

--- app/controllers/liquor_stores_controller.rb
+++ app/controllers/liquor_stores_controller.rb
@@ -1,2 +1,16 @@
-class LiquorStoresController < ApplicationController
+class LiqueursController < ApplicationController
+  def index
+    recipes = Recipe.all
+    render json: RecipeSerializer.new(recipes)
+  end
+
+  def show
+    recipe = Recipe.find_by(id: params[:id])
+    options = {
+      include: [:mixers, :garnishes, :liquors, :liqueurs]
+    }
+    render json: RecipeSerializer.new(recipe, options)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -18,5 +18,6 @@ Rails.application.routes.draw do
   resources :ingedients
   resources :recipes
   resources :users
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

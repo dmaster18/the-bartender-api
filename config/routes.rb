b01989a184e669eabf1c87527f27c23179ae7f86
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-30 23:19:13 -0400

    Commit #117

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,5 @@ Rails.application.routes.draw do
   resources :recipes
   resources :users
 
-  get '/patients/:id', to: 'patients#show'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

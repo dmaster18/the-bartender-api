9cb4e8cf4fd97a2bf3c259f1b3233db31dd0622d
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-10-31 22:49:33 -0400

    Commit #124

--- index.js
+++ index.js
@@ -19,7 +19,23 @@ document.addEventListener('DOMContentLoaded', function() {
   fetchRecipes();
 })
 
-document.addEventListener('click', function() {
-  console.log('DOM fully loaded and parsed')
-  fetchRecipes();
-})
+const recipeCard(json){
+
+  json.forEach(recipe=> {
+    const recipeCard = document.createElement()
+
+
+
+  })
+
+  <div class="card">
+    <img src="img_avatar.png" alt="Avatar" style="width:100%">
+    <div class="container">
+      <h4><b>John Doe</b></h4>
+      <p>Architect & Engineer</p>
+    </div>
+  </div>
+
+
+
+}

0fcafcbc5b9680fdf401a0f5390757f144767fe1
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-01 16:16:32 -0500

    Commit #141

--- scripts/study.js
+++ scripts/study.js
@@ -7,30 +7,34 @@ function fetchRecipes() {
 
 function renderRecipeCards(json){
   json["data"].forEach(recipe => {
-    const main = document.querySelector('main')
-    const recipeCard = document.createElement('div.card')  
-    recipeCard.innerHTML = `<div.container><h2><b>${recipe.attributes.name}</b></h2></div>`
+    const main = document.querySelector('main');
+    const recipeCard = document.createElement('div.card');
+    recipeCard.innerHTML = `<div.container><h2><b>${recipe.attributes.name}</b></h2></div>`;
     if (recipe.attributes.liquors) {
-      const liquors = recipe.attributes.liquors.forEach(liquor => `<li.liquor>${liquor}</li>`)
-      const liquorsElement = `<p><b><u>Liquors:</u></b><ol>${liquors}</ol></p>`
-      recipeCard.appendChild(liquorsElement)
+      const liquors = recipe.attributes.liquors.forEach(liquor => `<li.liquor>${liquor.name}</li>`);
+      const liquorsElement = document.createElement('p.liquors')
+      liquorsElement.innerHTML = `<b><u>Liquors:</u> </b><ol>${liquors}</ol>`;
+      recipeCard.appendChild(liquorsElement);
     }
     if (recipe.attributes.liqueurs) {
-      const liqueurs = recipe.attributes.liqueurs.forEach(liqueur => `<li.liqueur>${liqueur}</li>`)
-      const liqueursElement = `<p><b><u>Liqueurs:</u></b><ol>${liqueurs}</ol></p>`
-      recipeCard.appendChild(liqueursElement)
+      const liqueur = recipe.attributes.liqueurs.forEach(liqueur => `<li.liqueur>${liqueur.name}</li>`);
+      const liqueursElement = document.createElement('p.liqueurs')
+      liqueursElement.innerHTML = `<b><u>Liqueurs:</u> </b><ol>${liqueurs}</ol>`;
+      recipeCard.appendChild(liqueursElement);
     }
     if (recipe.attributes.mixers) {
-      const mixers = recipe.attributes.mixers.forEach(mixer => `<li.mixer>${mixer}</li>`)
-      const mixersElement = `<p><b><u>Mixers:</u></b><ol>${mixers}</ol></p>`
-      recipeCard.appendChild(mixersElement)
+      const mixers = recipe.attributes.mixers.forEach(mixer => `<li.mixer>${mixer.name}</li>`);
+      const mixersElement = document.createElement('p.mixers')
+      mixersElement.innerHTML = `<b><u>Mixers:</u> </b><ol>${mixers}</ol>`;
+      recipeCard.appendChild(mixersElement);
     }
     if (recipe.attributes.garnishes) {
-      const garnishes = recipe.attributes.garnishes.forEach(garnish => `<li.garnish>${garnish}</li>`)
-      const garnishesElement = `<p><b><u>Garnishes:</u></b><ol>${garnishes}</ol></p>`
-      recipeCard.appendChild(garnishesElement)
+      const garnishes = recipe.attributes.garnishes.forEach(garnish => `<li.garnish>${garnish.name}</li>`);
+      const garnishesElement = document.createElement('p.garnishes')
+      liquorsElement.innerHTML = `<b><u>Garnishes:</u> </b><ol>${garnishes}</ol>`;
+      recipeCard.appendChild(garnishesElement);
     }
-    main.appendChild(recipeCard)
+    main.appendChild(recipeCard);
   })
 }
 

48382ec09c4de3e3e31c8a1ca91744acc92346d2
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-01 00:03:05 -0400

    Commit #128

--- index.js
+++ index.js
@@ -24,26 +24,24 @@ function recipeCard(json){
     const recipeCard = document.createElement("div.card")
     recipeCard.innerHTML = `<div.container><h2><b>${recipe.name}</b></h2><p>${liquorsElement}</p><p>liqueursElement</p><p>mixersElement</p><p>garnishesElement</p></div>`
     const liquorsElement = `<p><b><u>Liquors:</u></b><ol>${liquors}</ol></p>`
-    const liquors = recipe.liquors.forEach(liquor => `<li>${liquor}</li>`)
+    const liquors = recipe.liquors.forEach(liquor => `<li.liquor>${liquor}</li>`)
     const liqueursElement = `<p><b><u>Liqueurs:</u></b><ol>${liqueurs}</ol></p>`
-    const liqueurs = recipe.liqueurs.forEach(liqueur => `<li>${liqueur}</li>`)
+    const liqueurs = recipe.liqueurs.forEach(liqueur => `<li.liqueur>${liqueur}</li>`)
     const mixersElement = `<p><b><u>Mixers:</u></b><ol>${mixers}</ol></p>`
-    const mixers = recipe.mixers.forEach(mixer => `<li>${mixer}</li>`)
+    const mixers = recipe.mixers.forEach(mixer => `<li.mixer>${mixer}</li>`)
     const garnishesElement = `<p><b><u>Garnishes:</u></b><ol>${garnishes}</ol></p>`
-    const garnishes = recipe.garnishes.forEach(garnish => `<li>${garnish}</li>`)
+    const garnishes = recipe.garnishes.forEach(garnish => `<li.garnish>${garnish}</li>`)
   })
 }
 
-  <div class="card">
-    <img src="img_avatar.png" alt="Avatar" style="width:100%">
-    <div class="container">
-      <h4><b>John Doe</b></h4>
-      <p>Architect & Engineer</p>
-    </div>
-  </div>
-
-
-  header.innerHTML = "<h1>Poodles!</h1><h3>An Essay into the Pom-Pom as Aesthetic Reconfiguration of the Other from a post-Frankfurt School Appropriationist Perspective</h3><p><em>By: Byron Q. Poodle, Esq., BA.</em></p>";
-
+function liquorCorrect?(question) {
+  const recipeCard = document.querySelector("div.card")
+  liquorElement = document.querySelector("li.liquor")
+  if (recipe.liquors.includes(user_input) {
+    liquorElement.style.backgroundColor = "green"
+  } else {
+    liquorElement.style.backgroundColor = "red"
+  }
+}
 
 }

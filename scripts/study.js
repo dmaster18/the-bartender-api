179a3c589b1a7a00c6c537b1ac4edb801b73fd30
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-02 17:59:48 -0500

    Commit #168

--- scripts/study.js
+++ scripts/study.js
@@ -1,92 +1,94 @@
-function generateIngredientCard(recipe) { //To generated Cocktail Recipe Ingredient Index Card in HTML
-  const ingredientCard = document.createElement('div.card')
+function generatecard(recipe) { //To generated Cocktail Recipe Ingredient Index Card in HTML
   const main = document.querySelector('main');
-  ingredientCard.innerHTML = `<div.container><h2><b>${recipe.attributes.name}</b></h2></div>`
+  const card = document.createElement('div.card')
+  const cardFront = document.createElement('div.card-front')
+  const cardBack = document.createElement('div.card-back')
+  card.innerHTML = `<div.card-front><h2><b>${recipe.attributes.name}</b></h2></div>`
   if (recipe.attributes.liquors.length > 0) {
     const liquorsTitle = document.createElement('h4.liquorsTitle')
     liquorsTitle.innerHTML = '<b><u>Liquors</u></b>'
-    ingredientCard.appendChild(liquorsTitle)
+    card.appendChild(liquorsTitle)
     const liquorsElement = document.createElement('ul.liquors')
     const liquors = recipe.attributes.liquors.forEach( liquor =>
       {const liquor_line = document.createElement('li.liquor')
       liquor_line.innerHTML = `<li>${liquor.name}</li>`
       liquorsElement.appendChild(liquor_line)
     })
-    ingredientCard.appendChild(liquorsElement);
+    card.appendChild(liquorsElement);
   }
   if (recipe.attributes.liqueurs.length > 0) {
     const liqueursTitle = document.createElement('h4.liqueursTitle')
     liqueursTitle.innerHTML = '<b><u>Liqueurs</u></b>'
-    ingredientCard.appendChild(liqueursTitle)
+    card.appendChild(liqueursTitle)
     const liqueursElement = document.createElement('ul.liqueurs')
     const liqueurs = recipe.attributes.liqueurs.forEach( liqueur =>
       {const liqueur_line = document.createElement('li.liqueur')
       liqueur_line.innerHTML = `<li>${liqueur.name}</li>`
       liqueursElement.appendChild(liqueur_line)
     })
-    ingredientCard.appendChild(liqueursElement);
+    card.appendChild(liqueursElement);
   }
   if (recipe.attributes.mixers.length > 0) {
     const mixersTitle = document.createElement('h4.mixersTitle')
     mixersTitle.innerHTML = '<b><u>Mixers</u></b>'
-    ingredientCard.appendChild(mixersTitle)
+    card.appendChild(mixersTitle)
     const mixersElement = document.createElement('ul.mixers')
     const mixers = recipe.attributes.mixers.forEach( mixer =>
       {const mixer_line = document.createElement('li.mixer')
       mixer_line.innerHTML = `<li>${mixer.name}</li>`
       mixersElement.appendChild(mixer_line)
     })
-    ingredientCard.appendChild(mixersElement);
+    card.appendChild(mixersElement);
   }
   if (recipe.attributes.garnishes.length > 0) {
     const garnishesTitle = document.createElement('h4.garnishesTitle')
     garnishesTitle.innerHTML = '<b><u>Garnishes</u></b>'
-    ingredientCard.appendChild(garnishesTitle)
+    card.appendChild(garnishesTitle)
     const garnishesElement = document.createElement('ul.garnishes')
     const garnishes = recipe.attributes.garnishes.forEach( garnish =>
       {const garnish_line = document.createElement('li.garnish')
       garnish_line.innerHTML = `<li>${garnish.name}</li>`
       garnishesElement.appendChild(garnish_line)
     })
-    ingredientCard.appendChild(garnishesElement);
+    card.appendChild(garnishesElement);
   }
-  main.appendChild(ingredientCard);
+  main.appendChild(card);
 }
 
 
-function fetchIngredientCards() {
+function fetchcards() {
   const recipes_url = "http://127.0.0.1:3000/recipes"
   return fetch(recipes_url)
   .then(resp => resp.json())
-  .then(json => renderIngredientCards(json))
+  .then(json => rendercards(json))
 }
 
-function renderIngredientCards(json) { //Renders all Cocktail Recipe Ingredient Index Cards
-  json["data"].forEach(recipe => generateIngredientCard(recipe))
+function rendercards(json) { //Renders all Cocktail Recipe Ingredient Index Cards
+  json["data"].forEach(recipe => generatecard(recipe))
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM fully loaded and parsed');
-  fetchIngredientCards();
+  fetchcards();
 })
 /*
-function fetchRandomIngredientCard() {
+function fetchRandomcard() {
   const recipes_url = "http://127.0.0.1:3000/recipes"
   return fetch(recipes_url)
   .then(resp => resp.json())
-  .then(json => renderRandomIngredientCard(json))
+  .then(json => renderRandomcard(json))
 }
 
-function renderRandomIngredientCard(json) { //Renders random Cocktail Recipe Ingredient Index Card
+function renderRandomcard(json) { //Renders random Cocktail Recipe Ingredient Index Card
   const randomNumber = Math.floor(Math.random() * 508);
   const recipe = json["data"][randomNumber];
   const main = document.querySelector('main');
-  generateIngredientCard(recipe);
+  generatecard(recipe);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
   console.log('DOM fully loaded and parsed');
-  fetchRandomIngredientCard();
+  fetchRandomcard();
 })*/
 
 

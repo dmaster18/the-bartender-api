198a4d5e48e662b00e3a46d2e7bfdb5f1b4a3f01
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-02 18:59:06 -0500

    Commit #175

--- front_end/scripts/study.js
+++ front_end/scripts/study.js
@@ -10,12 +10,15 @@ function generatecard(recipe) { //To generated Cocktail Recipe Ingredient Index 
   cardBack.classList.add('card-back');
   cardFront.innerHTML = `<div.card-front><h2><b>${recipe.attributes.name}</b></h2></div>`
   if (recipe.attributes.liquors.length > 0) {
-    const liquorsTitle = document.createElement('h4.liquorsTitle')
-    liquorsTitle.innerHTML = '<b><u>Liquors</u></b>'
+    const liquorsTitle = document.createElement('h4');
+    liquorsTitle.classList.add('liquors-title');
+    liquorsTitle.innerHTML = '<b><u>Liquors</u></b>';
     cardBack.appendChild(liquorsTitle)
-    const liquorsElement = document.createElement('ul.liquors')
+    const liquorsElement = document.createElement('ul')
+    liquorsElement.classList.add('liquors');
     const liquors = recipe.attributes.liquors.forEach( liquor =>
-      {const liquor_line = document.createElement('li.liquor')
+      {const liquor_line = document.createElement('li');
+      liquor_line.classList.add('liquor');
       liquor_line.innerHTML = `<li>${liquor.name}</li>`
       liquorsElement.appendChild(liquor_line)
     })

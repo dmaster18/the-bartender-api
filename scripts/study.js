d914ed1cb162d265ac3b0d9eb31467ea089fa2b6
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-02 16:21:51 -0500

    Commit #163

--- scripts/study.js
+++ scripts/study.js
@@ -71,13 +71,64 @@ function fetchRandomIngredientCard() {
   const recipes_url = "http://127.0.0.1:3000/recipes"
   return fetch(recipes_url)
   .then(resp => resp.json())
-  .then(json => generateRandomIngredientCard(json))
+  .then(json => renderRandomIngredientCard(json))
 }
 
-function generateRandomIngredientCard(json) {
+function renderRandomIngredientCard(json) {
   const randomNumber = Math.floor(Math.random() * 508);
-  const randomIngredientCard = json["data"][randomNumber];
-  randomIngredientCard;
+  const recipe = json["data"][randomNumber];
+  const main = document.querySelector('main')
+  const ingredientCard = document.createElement('div.card')
+  ingredientCard.innerHTML = `<div.container><h2><b>${recipe.attributes.name}</b></h2></div>`
+  if (recipe.attributes.liquors.length > 0) {
+    const liquorsTitle = document.createElement('h4.liquorsTitle')
+    liquorsTitle.innerHTML = '<b><u>Liquors</u></b>'
+    ingredientCard.appendChild(liquorsTitle)
+    const liquorsElement = document.createElement('ul.liquors')
+    const liquors = recipe.attributes.liquors.forEach( liquor =>
+      {const liquor_line = document.createElement('li.liquor')
+      liquor_line.innerHTML = `<li>${liquor.name}</li>`
+      liquorsElement.appendChild(liquor_line)
+    })
+    ingredientCard.appendChild(liquorsElement);
+  }
+  if (recipe.attributes.liqueurs.length > 0) {
+    const liqueursTitle = document.createElement('h4.liqueursTitle')
+    liqueursTitle.innerHTML = '<b><u>Liqueurs</u></b>'
+    ingredientCard.appendChild(liqueursTitle)
+    const liqueursElement = document.createElement('ul.liqueurs')
+    const liqueurs = recipe.attributes.liqueurs.forEach( liqueur =>
+      {const liqueur_line = document.createElement('li.liqueur')
+      liqueur_line.innerHTML = `<li>${liqueur.name}</li>`
+      liqueursElement.appendChild(liqueur_line)
+    })
+    ingredientCard.appendChild(liqueursElement);
+  }
+  if (recipe.attributes.mixers.length > 0) {
+    const mixersTitle = document.createElement('h4.mixersTitle')
+    mixersTitle.innerHTML = '<b><u>Mixers</u></b>'
+    ingredientCard.appendChild(mixersTitle)
+    const mixersElement = document.createElement('ul.mixers')
+    const mixers = recipe.attributes.mixers.forEach( mixer =>
+      {const mixer_line = document.createElement('li.mixer')
+      mixer_line.innerHTML = `<li>${mixer.name}</li>`
+      mixersElement.appendChild(mixer_line)
+    })
+    ingredientCard.appendChild(mixersElement);
+  }
+  if (recipe.attributes.garnishes.length > 0) {
+    const garnishesTitle = document.createElement('h4.garnishesTitle')
+    garnishesTitle.innerHTML = '<b><u>Garnishes</u></b>'
+    ingredientCard.appendChild(garnishesTitle)
+    const garnishesElement = document.createElement('ul.garnishes')
+    const garnishes = recipe.attributes.garnishes.forEach( garnish =>
+      {const garnish_line = document.createElement('li.garnish')
+      garnish_line.innerHTML = `<li>${garnish.name}</li>`
+      garnishesElement.appendChild(garnish_line)
+    })
+    ingredientCard.appendChild(garnishesElement);
+  }
+  main.appendChild(ingredientCard);
 }
 
 document.addEventListener('DOMContentLoaded', function() {

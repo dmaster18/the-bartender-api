22c0680e58d0a18a0165aa0475f46ee060e0879b
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-10 23:37:53 -0500

    Commit #461

--- front_end/scripts/quiz_intro.js
+++ front_end/scripts/quiz_intro.js
@@ -1,5 +1,5 @@
 const buttonListener = function() {document.querySelector('button').addEventListener("click", function() {
-  const values = document.querySelectorAll('input');
+  const values = [[.slice.call(document.querySelectorAll('input'));
   const selectedValues = values.filter(value => value.checked);
   console.log(selectedValues);
 })}

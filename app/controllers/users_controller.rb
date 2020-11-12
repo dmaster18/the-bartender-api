74f0b33975fe8b827c4256aa02d421bc8daa12e8
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-12 18:45:11 -0500

    Commit #572

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,9 @@
 class UsersController < ApplicationController
+  def index
+    @user = User.create(name: name, score: score, percentage: percentage)
+    @users = User.all
+    @users
+  end
+
+
 end

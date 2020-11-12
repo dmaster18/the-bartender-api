687739a15c7c4698243897a610ebfd1a313d359e
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-12 18:06:30 -0500

    Commit #568

--- db/migrate/20201027004523_create_users.rb
+++ db/migrate/20201027004523_create_users.rb
@@ -1,17 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
-      t.string :username
-      t.string :username_confirmation
-      t.string :first_name
-      t.string :last_name
-      t.string :email
-      t.string :password
-      t.string :password_confirmation
-      t.integer :uid
-      t.string :zip_code
-      t.boolean :password_required
-      t.text :quiz_score_history 
+      t.string :name
+      t.integer :score
+      t.integer :percentage
+
 
       t.timestamps
     end
--- db/migrate/20201027011251_create_liquor_stores.rb
+++
@@ -1,8 +0,0 @@
-class CreateLiquorStores < ActiveRecord::Migration[6.0]
-  def change
-    create_table :liquor_stores do |t|
-
-      t.timestamps
-    end
-  end
-end

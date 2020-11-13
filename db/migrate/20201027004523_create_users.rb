dea2468f065675c497cc7685f832435f92f99d8e
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-13 18:04:10 -0500

    Commit #609

--- db/migrate/20201027004523_create_users.rb
+++ db/migrate/20201027004523_create_users.rb
@@ -2,9 +2,8 @@ class CreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       t.string :name
-      t.integer :score
-      t.integer :percentage
-      t.integer :leaderboard_id
+      t.string :score
+      t.string :percentage
 
       t.timestamps
     end
--- db/migrate/20201112230755_create_leaderboards.rb
+++
@@ -1,8 +0,0 @@
-class CreateLeaderboards < ActiveRecord::Migration[6.0]
-  def change
-    create_table :leaderboards do |t|
-
-      t.timestamps
-    end
-  end
-end

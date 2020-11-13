c48a02f63ac15de27f14053e300d27ec2fabb4d9
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-13 18:26:26 -0500

    Commit #611

--- front_end/scripts/quiz_with_params.js
+++ front_end/scripts/quiz_with_params.js
@@ -208,6 +208,8 @@ const runQuestion = function(i, randomRecipes, quizScore) {
     const main = document.querySelector('main');
     main.innerHTML = '';
     const leaderboardForm = document.createElement('div');
-    leaderboardForm.innerHTML = '<form action="http://localhost:3000/createUser" method="POST"><label for="user[name]">Enter your name:</label><input type="text" name="user[name]" id="user[name]"><input type="hidden" id="user[score]" name="user[score]" value=`${quizScore}`><input type="hidden" id="user[percentage]" name="user[percentage]" value=`${percentage}`><button type="submit" value="Submit">Submit</button></form>'
+    leaderboardForm.innerHTML = '<form action="http://localhost:3000/createUser" method="POST"><label for="user[name]">Enter your name:</label><input type="text" name="user[name]" id="user[name]"><input type="hidden" id="user[score]" name="user[score]"><input type="hidden" id="user[percentage]" name="user[percentage]"><button type="submit" value="Submit">Submit</button></form>'
     main.appendChild(leaderboardForm);
+    document.getElementById("user[score]").value = `${quizScore}`
+    document.getElementById("user[percentage]").value = `${percentage}`
   }

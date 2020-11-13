170a8f5e3f57dbb1f5fcb8d24c727e650bcb71c2
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-12 19:16:32 -0500

    Commit #578

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  def new
+    user = User.new
+  end
+
+  def create
+    user = User.create(user_params)
+    redirect_to :index
+  end
+
   def index
     users = User.all
     render json: UserSerializer.new(users)

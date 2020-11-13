4fb5af50fe4abec04f19f212b6fdcbf3ae7a5545
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-13 16:34:30 -0500

    Commit #598

--- front_end/scripts/leaderboard.js
+++ front_end/scripts/leaderboard.js
@@ -18,8 +18,25 @@ const generateGIF = function() {
 const generateLeaderboard(json) = function() {
   const userData = json['data'];
   const users = userData.map(user => user.name = )
-
-
+  const leaderboard = document.createElement('table');
+  leaderboardHeader = '<tr><th>Name</th><th>Score</th><th>Percentage</th></tr><tr>'
+  leaderboardData = 
+  leaderboard.innerHTML = (
+      <td>Jill</td>
+      <td>Smith</td>
+      <td>50</td>
+    </tr>
+    <tr>
+      <td>Eve</td>
+      <td>Jackson</td>
+      <td>94</td>
+    </tr>
+    <tr>
+      <td>John</td>
+      <td>Doe</td>
+      <td>80</td>
+    </tr>
+  </table>
 
 }
 

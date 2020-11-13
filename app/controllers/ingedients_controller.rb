76e0f65e4f7731d828a06715048cfba8c2d6bcca
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-12 19:05:03 -0500

    Commit #576

--- app/controllers/ingedients_controller.rb
+++
@@ -1,2 +0,0 @@
-class IngedientsController < ApplicationController
-end
--- app/models/ingedient.rb
+++
@@ -1,8 +0,0 @@
-class Ingedient < ApplicationRecord
-  has_many :recipe_ingredients
-  has_many :recipes, through: :recipe_ingredients
-
-  validates :name, presence: true
-  validates :name, uniqueness: true
-
-end

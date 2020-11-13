b8f411aadf69ec2a10cd9b9cef4e6ed6434bb981
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-13 14:21:37 -0500

    Commit #585

--- front_end/scripts/quiz_with_params.js
+++ front_end/scripts/quiz_with_params.js
@@ -210,6 +210,7 @@ const runQuestion = function(i, randomRecipes, quizScore) {
     const main = document.querySelector('main');
     main.innerHTML = '';
     const leaderboardForm = document.createElement('div');
-    leaderboardForm.innerHTML = '<form action="http://localhost:3000/users/create" method="POST"><input type="text" name="user[name]" value="name"> <button type="submit" value="Submit">Submit</button></form?'
+    leaderboardForm.innerHTML = '<form action=users_path method="POST"><input type="text" name="user[name]" value="name"> <button type="submit" value="Submit">Submit</button></form?'
     main.appendChild(leaderboardForm);
   }
+  //"http://localhost:3000/users/create"

cd13259f2e95f11ff195353b9841e9410f4676d4
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-13 23:05:53 -0500

    Commit #619

--- front_end/scripts/leaderboard.js
+++ front_end/scripts/leaderboard.js
@@ -21,7 +21,7 @@ const generateLeaderboard = function(json) {
   main.innerHTML = ''
   const userData = json['data'];
   const leaderboard = document.createElement('table');
-  leaderboard.classList.add('sortable')
+  leaderboard.classList.add('leaderboard')
   const leaderboardHeader = '<tr><th>Name</th><th>Score</th><th>Percentage</th></tr><tr>'
   const arrayOfUserData = userData.map(user => `<tr><td>${user.attributes.name}</td> <td>${user.attributes.score}</td> <td>${user.attributes.percentage}</td></tr>`);
   arrayOfUserData = arrayOfUserData.sort((x, y) => (parseInt(x.attributes.percentage) > parseInt(y.attributes.percentage)) ? 1 : -1)

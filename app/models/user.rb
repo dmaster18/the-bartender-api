53c25836df8509840a0ee4b50bfbe8eed7747634
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-13 23:18:26 -0500

    Commit #620

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  before_save :order_by_percentage, :order_by_score, :order_by_name
+
+  def order_by_percentage
+    User.order(percentage: :desc)
+  end
+
+  def order_by_score
+    User.order(score: :desc)
+  end
+
+  def order_by_name
+    User.order(:name)
+  end
+
 end

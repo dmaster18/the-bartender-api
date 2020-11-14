d8f5339a0abccc41e1b348fe1854368a5b2eb213
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-13 20:55:50 -0500

    Commit #618

--- front_end/scripts/leaderboard.js
+++ front_end/scripts/leaderboard.js
@@ -17,12 +17,14 @@ const generateGIF = function() {
 }
 
 const generateLeaderboard = function(json) {
-  const userData = json['data'];
   const main = document.querySelector('main');
+  main.innerHTML = ''
+  const userData = json['data'];
   const leaderboard = document.createElement('table');
-  leaderboard.classList.add('leaderboard')
+  leaderboard.classList.add('sortable')
   const leaderboardHeader = '<tr><th>Name</th><th>Score</th><th>Percentage</th></tr><tr>'
   const arrayOfUserData = userData.map(user => `<tr><td>${user.attributes.name}</td> <td>${user.attributes.score}</td> <td>${user.attributes.percentage}</td></tr>`);
+  arrayOfUserData = arrayOfUserData.sort((x, y) => (parseInt(x.attributes.percentage) > parseInt(y.attributes.percentage)) ? 1 : -1)
   const leaderboardData = arrayOfUserData.join(' ');
   leaderboard.innerHTML = `${leaderboardHeader} ${leaderboardData}`
   main.appendChild(leaderboard);

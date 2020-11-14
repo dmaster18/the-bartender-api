3c7d101f76010f0b0c1fc60037fc13169da17da5
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-13 23:41:11 -0500

    Commit #625

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,6 +1,5 @@
 class UserSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :score, :percentage
-
-
+  attributes :name, :score, :percentage, :order_by_name, :order_by_score, :order_by_percentage
+  
 end

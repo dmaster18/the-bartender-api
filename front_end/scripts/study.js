90caa8ba607c96674cf5f45f6d5cafbee681554c
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-14 16:35:26 -0500

    Commit #677

--- front_end/scripts/study.js
+++ front_end/scripts/study.js
@@ -159,6 +159,64 @@ function generatecard(recipe) { //To generated Cocktail Recipe Ingredient Index 
   cardBackIngredientsButtons.appendChild(backRevealFullRecipeButton);
   cardBackIngredients.appendChild(cardBackIngredientsButtons);
 
+  const cardBackFullRecipe = document.createElement('div');
+  cardBackFullRecipe.classList.add('card-back-full-recipe');
+  if (recipe.attributes.liquors_array.length > 0) {
+    const liquorsTitle = document.createElement('h4');
+    liquorsTitle.classList.add('liquors-title');
+    liquorsTitle.innerHTML = '<b><u>Liquors</u></b>';
+    cardBackFullRecipe.appendChild(liquorsTitle)
+    const liquorsElement = document.createElement('ul')
+    liquorsElement.classList.add('liquors');
+    const liquors = recipe.attributes.liquors_array.forEach( liquor =>
+      {const liquor_line = document.createElement('li');
+      liquor_line.classList.add('liquor');
+      liquor_line.innerHTML = `<li>${liquor.name}</li>`
+      liquorsElement.appendChild(liquor_line)
+    })
+    cardBackFullRecipe.appendChild(liquorsElement);
+  }
+  if (recipe.attributes.liqueurs_array.length > 0) {
+    const liqueursTitle = document.createElement('h4.liqueursTitle')
+    liqueursTitle.innerHTML = '<b><u>Liqueurs</u></b>'
+    cardBackFullRecipe.appendChild(liqueursTitle)
+    const liqueursElement = document.createElement('ul.liqueurs')
+    const liqueurs = recipe.attributes.liqueurs_array.forEach( liqueur =>
+      {const liqueur_line = document.createElement('li.liqueur')
+      liqueur_line.innerHTML = `<li>${liqueur.name}</li>`
+      liqueursElement.appendChild(liqueur_line)
+    })
+    cardBackFullRecipe.appendChild(liqueursElement);
+  }
+  if (recipe.attributes.mixers_array.length > 0) {
+    const mixersTitle = document.createElement('h4.mixersTitle')
+    mixersTitle.innerHTML = '<b><u>Mixers</u></b>'
+    cardBackFullRecipe.appendChild(mixersTitle)
+    const mixersElement = document.createElement('ul.mixers')
+    const mixers = recipe.attributes.mixers_array.forEach( mixer =>
+      {const mixer_line = document.createElement('li.mixer')
+      mixer_line.innerHTML = `<li>${mixer.name}</li>`
+      mixersElement.appendChild(mixer_line)
+    })
+    cardBackFullRecipe.appendChild(mixersElement);
+  }
+  if (recipe.attributes.garnishes_array.length > 0) {
+    const garnishesTitle = document.createElement('h4.garnishesTitle')
+    garnishesTitle.innerHTML = '<b><u>Garnishes</u></b>'
+    cardBackFullRecipe.appendChild(garnishesTitle)
+    const garnishesElement = document.createElement('ul.garnishes')
+    const garnishes = recipe.attributes.garnishes_array.forEach( garnish =>
+      {const garnish_line = document.createElement('li.garnish')
+      garnish_line.innerHTML = `<li>${garnish.name}</li>`
+      garnishesElement.appendChild(garnish_line)
+    })
+    cardBackFullRecipe.appendChild(garnishesElement);
+  }
+
+
+
+
+
   card.appendChild(cardFront);
   revealIngredientsButton.addEventListener('click', function() {revealIngredients(card, cardBackIngredients)})
   showRecipeNameButton.addEventListener('click', function() {showRecipeName(card, cardFront)})

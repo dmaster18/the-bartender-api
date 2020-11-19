39814a204603c84ceaf62df3d945de6627a9dd40
David Strauch <50710431+dmaster18@users.noreply.github.com>
2020-11-19 12:10:41 -0500

    11-19-20 Commit #2

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: true
 
-  #serialize :order_by_percentage, Array
-  #serialize :order_by_score, Array
-  #serialize :order_by_name, Array
-
-  #after_save :order_by_percentage, :order_by_score, :order_by_name
 
   def self.order_by_percentage
     User.order(percentage: :desc)

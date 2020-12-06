d8a7cb1cf80cc11cff37543c62073b81ff20627f
David <dmaster18@gmail.com>
2020-12-06 16:02:33 -0500

    Updated leaderboard.js linter errors

--- front_end/scripts/leaderboard.js
+++ front_end/scripts/leaderboard.js
@@ -1,43 +1,40 @@
 class Leaderboard {
-  constructor() {}
-
-  fetchUsers() {
-    const users_url = 'http://127.0.0.1:3000/users';
-    return fetch(users_url)
-    .then(resp => resp.json())
+  fetchUsers () {
+    const usersURL = 'http://127.0.0.1:3000/users';
+    return fetch(usersURL).then(resp => resp.json());
   }
 
-  renderLoadingState() {
+  renderLoadingState () {
     const main = document.querySelector('main');
-    main.innerHTML = ''
+    main.innerHTML = '';
     const gifContainer = document.createElement('div');
     gifContainer.classList.add('gif-container');
     const gifElement = document.createElement('img');
-    gifElement.src = '../gifs/old_bartender.gif'
+    gifElement.src = '../gifs/old_bartender.gif';
     gifContainer.appendChild(gifElement);
     main.appendChild(gifContainer);
   }
 
-  renderLeaderboard(json) {
+  renderLeaderboard (json) {
     const main = document.querySelector('main');
-    const userData = json['data'];
+    const userData = json.data;
     const leaderboard = document.createElement('table');
-    leaderboard.classList.add('leaderboard')
-    const leaderboardHeader = '<tr><th>Name</th><th>Score</th><th>Percentage</th></tr><tr>'
+    leaderboard.classList.add('leaderboard');
+    const leaderboardHeader = '<tr><th>Name</th><th>Score</th><th>Percentage</th></tr><tr>';
     const arrayOfUserData = userData.map(user => `<tr><td>${user.attributes.name}</td> <td>${user.attributes.score} Points</td> <td>${user.attributes.percentage}%</td></tr>`);
     const leaderboardData = arrayOfUserData.join(' ');
-    leaderboard.innerHTML = `${leaderboardHeader} ${leaderboardData}`
+    leaderboard.innerHTML = `${leaderboardHeader} ${leaderboardData}`;
     main.appendChild(leaderboard);
   }
 
-  render() {
+  render () {
     const main = document.querySelector('main');
     this.renderLoadingState();
     main.innerHTML = '';
-    this.fetchUsers().then(json => this.renderLeaderboard(json))
+    this.fetchUsers().then(json => this.renderLeaderboard(json));
   }
 }
 
 const leaderBoard = new Leaderboard();
 
-window.addEventListener('DOMContentLoaded', function() {leaderBoard.render()});
+window.addEventListener('DOMContentLoaded', function () { leaderBoard.render(); });

179181a0a41dd14dac415dfa21b1e4fe2b3da2fa
David <dmaster18@gmail.com>
2020-12-06 16:56:01 -0500

    Updated study.js to correct linter errors

--- front_end/scripts/study.js
+++ front_end/scripts/study.js
@@ -1,43 +1,43 @@
 class Study {
-  constructor() {
+  constructor () {
     this.randomNumbers = [];
     this.i = 0;
     this.limit = 50;
     this.offset = 0;
   }
 
-  fetchCards() {
-      const recipes_url = `http://127.0.0.1:3000/recipes/?limit=${this.limit}&offset=${this.offset}`;
-      return fetch(recipes_url).then(resp => resp.json());
+  fetchCards () {
+    const recipesURL = `http://127.0.0.1:3000/recipes/?limit=${this.limit}&offset=${this.offset}`;
+    return fetch(recipesURL).then(resp => resp.json());
   }
 
-  render() {
+  render () {
     const main = document.querySelector('main');
     this.renderLoadingState(main);
     this.fetchCards().then(json => {
       this.randomNumbers = this.randomNumberGenerator(json.data.length);
-      this.randomRecipeCard(json)
-    })
+      this.randomRecipeCard(json);
+    });
   }
 
-  renderLoadingState(main) {
+  renderLoadingState (main) {
     const gifElement = document.createElement('img');
-    gifElement.src = '../gifs/dog_bartender.gif'
+    gifElement.src = '../gifs/dog_bartender.gif';
     main.appendChild(gifElement);
   }
 
-  randomNumberGenerator(recipeCardCount) {
-    for(let i = 0; i < recipeCardCount; i++) {
+  randomNumberGenerator (recipeCardCount) {
+    for (let i = 0; i < recipeCardCount; i++) {
       this.randomNumbers.push(i);
     }
     return this.shuffle();
   }
 
-  shuffle() {
+  shuffle () {
     return this.randomNumbers.sort(() => Math.random() - 0.5);
   }
 
-  randomRecipeCard(json, i) {
+  randomRecipeCard (json, i) {
     const main = document.querySelector('main');
     main.innerHTML = '';
     const recipeCards = this.generateCards(json);
@@ -47,13 +47,13 @@ class Study {
     this.createButtons(json, main);
   }
 
-  generateCards(json) { //Renders all Cocktail Recipe Ingredient Index Cards
-    const recipeData = json['data'];
-    const recipeCards = recipeData.map(recipe => this.generatecard(recipe))
+  generateCards (json) {
+    const recipeData = json.data;
+    const recipeCards = recipeData.map(recipe => this.generatecard(recipe));
     return recipeCards;
   }
 
-  createButtons(json, main) {
+  createButtons (json, main) {
     const buttons = document.createElement('div');
     buttons.classList.add('buttons');
     const nextCardButton = document.createElement('button');
@@ -66,40 +66,40 @@ class Study {
     buttons.appendChild(previousCardButton);
     buttons.appendChild(nextCardButton);
     main.appendChild(buttons);
-    nextCardButton.addEventListener('click', () => {this.nextRecipeCard(json)});
-    previousCardButton.addEventListener('click', () => {this.previousRecipeCard(json)});
+    nextCardButton.addEventListener('click', () => { this.nextRecipeCard(json); });
+    previousCardButton.addEventListener('click', () => { this.previousRecipeCard(json); });
   }
 
-  nextRecipeCard(json) {
+  nextRecipeCard (json) {
     if (this.i === (this.randomNumbers.length - 1) && document.querySelector('.next-study-session') === null) {
       const main = document.querySelector('main');
       const nextStudySession = document.createElement('button');
-      nextStudySession.classList.add('next-study-session')
+      nextStudySession.classList.add('next-study-session');
       nextStudySession.innerHTML = 'Study More?';
-      nextStudySession.addEventListener('click', () => {main.innerHTML = ''; this.offset +=50;this.render()})
+      nextStudySession.addEventListener('click', () => { main.innerHTML = ''; this.offset += 50; this.render(); });
       const divButtons = document.querySelector('div.buttons');
       divButtons.appendChild(nextStudySession);
-    } else if (this.i < (this.randomNumbers.length -1)) {
+    } else if (this.i < (this.randomNumbers.length - 1)) {
       this.i++; this.randomRecipeCard(json);
     }
   }
 
-  previousRecipeCard(json) {
-    if (this.i > this.offset) {this.i--;}
+  previousRecipeCard (json) {
+    if (this.i > this.offset) { this.i--; }
     this.randomRecipeCard(json);
   }
 
-  generatecard(recipe) { //To generate Cocktail Recipe Ingredient Index Card in HTML
+  generatecard (recipe) {
     const card = document.createElement('div');
-    card.classList.add('card')
+    card.classList.add('card');
     const cardFront = document.createElement('div');
     cardFront.classList.add('card-front');
-    cardFront.innerHTML = `<div.card-front><h2><b>${recipe.attributes.name}</b></h2></div>`
+    cardFront.innerHTML = `<div.card-front><h2><b>${recipe.attributes.name}</b></h2></div>`;
     const revealAnswer = document.createElement('div');
     revealAnswer.classList.add('reveal-answer');
     const revealIngredientsButton = document.createElement('button');
     revealIngredientsButton.classList.add('reveal-ingredients');
-    revealIngredientsButton.innerText = 'Reveal Ingredients Only'
+    revealIngredientsButton.innerText = 'Reveal Ingredients Only';
     const revealFullRecipeButton = document.createElement('button');
     revealFullRecipeButton.classList.add('reveal-full-recipe');
     revealFullRecipeButton.innerText = 'Reveal Ingredients + Measurements';
@@ -108,54 +108,54 @@ class Study {
     cardFront.appendChild(revealAnswer);
 
     const cardBackIngredients = document.createElement('div');
-    cardBackIngredients.innerHTML = `<div.card-back-title><h2><b>${recipe.attributes.name}</b></h2></div>`
+    cardBackIngredients.innerHTML = `<div.card-back-title><h2><b>${recipe.attributes.name}</b></h2></div>`;
     cardBackIngredients.classList.add('card-back-ingredients');
     if (recipe.attributes.liquors.length > 0) {
-      const liquorsTitle = document.createElement('h4.liquorsTitle')
-      liquorsTitle.innerHTML = '<b><u>Liquors</u></b>'
-      cardBackIngredients.appendChild(liquorsTitle)
-      const liquorsElement = document.createElement('ul.liquors')
-      const liquors = recipe.attributes.liquors.forEach( liquor =>
-        {const liquor_line = document.createElement('li.liquor')
-        liquor_line.innerHTML = `<li>${liquor.name}</li>`
-        liquorsElement.appendChild(liquor_line)
-      })
+      const liquorsTitle = document.createElement('h4.liquorsTitle');
+      liquorsTitle.innerHTML = '<b><u>Liquors</u></b>';
+      cardBackIngredients.appendChild(liquorsTitle);
+      const liquorsElement = document.createElement('ul.liquors');
+      const liquors = recipe.attributes.liquors.forEach(liquor => {
+        const liquorLine = document.createElement('li.liquor');
+        liquorLine.innerHTML = `<li>${liquor.name}</li>`;
+        liquorsElement.appendChild(liquorLine);
+      });
       cardBackIngredients.appendChild(liquorsElement);
     }
     if (recipe.attributes.liqueurs.length > 0) {
-      const liqueursTitle = document.createElement('h4.liqueursTitle')
-      liqueursTitle.innerHTML = '<b><u>Liqueurs</u></b>'
-      cardBackIngredients.appendChild(liqueursTitle)
-      const liqueursElement = document.createElement('ul.liqueurs')
-      const liqueurs = recipe.attributes.liqueurs.forEach( liqueur =>
-        {const liqueur_line = document.createElement('li.liqueur')
-        liqueur_line.innerHTML = `<li>${liqueur.name}</li>`
-        liqueursElement.appendChild(liqueur_line)
-      })
+      const liqueursTitle = document.createElement('h4.liqueursTitle');
+      liqueursTitle.innerHTML = '<b><u>Liqueurs</u></b>';
+      cardBackIngredients.appendChild(liqueursTitle);
+      const liqueursElement = document.createElement('ul.liqueurs');
+      const liqueurs = recipe.attributes.liqueurs.forEach(liqueur => {
+        const liqueurLine = document.createElement('li.liqueur');
+        liqueurLine.innerHTML = `<li>${liqueur.name}</li>`;
+        liqueursElement.appendChild(liqueurLine);
+      });
       cardBackIngredients.appendChild(liqueursElement);
     }
     if (recipe.attributes.mixers.length > 0) {
-      const mixersTitle = document.createElement('h4.mixersTitle')
-      mixersTitle.innerHTML = '<b><u>Mixers</u></b>'
-      cardBackIngredients.appendChild(mixersTitle)
-      const mixersElement = document.createElement('ul.mixers')
-      const mixers = recipe.attributes.mixers.forEach( mixer =>
-        {const mixer_line = document.createElement('li.mixer')
-        mixer_line.innerHTML = `<li>${mixer.name}</li>`
-        mixersElement.appendChild(mixer_line)
-      })
+      const mixersTitle = document.createElement('h4.mixersTitle');
+      mixersTitle.innerHTML = '<b><u>Mixers</u></b>';
+      cardBackIngredients.appendChild(mixersTitle);
+      const mixersElement = document.createElement('ul.mixers');
+      const mixers = recipe.attributes.mixers.forEach(mixer => {
+        const mixerLine = document.createElement('li.mixer');
+        mixerLine.innerHTML = `<li>${mixer.name}</li>`;
+        mixersElement.appendChild(mixerLine);
+      });
       cardBackIngredients.appendChild(mixersElement);
     }
     if (recipe.attributes.garnishes.length > 0) {
-      const garnishesTitle = document.createElement('h4.garnishesTitle')
-      garnishesTitle.innerHTML = '<b><u>Garnishes</u></b>'
-      cardBackIngredients.appendChild(garnishesTitle)
-      const garnishesElement = document.createElement('ul.garnishes')
-      const garnishes = recipe.attributes.garnishes.forEach( garnish =>
-        {const garnish_line = document.createElement('li.garnish')
-        garnish_line.innerHTML = `<li>${garnish.name}</li>`
-        garnishesElement.appendChild(garnish_line)
-      })
+      const garnishesTitle = document.createElement('h4.garnishesTitle');
+      garnishesTitle.innerHTML = '<b><u>Garnishes</u></b>';
+      cardBackIngredients.appendChild(garnishesTitle);
+      const garnishesElement = document.createElement('ul.garnishes');
+      const garnishes = recipe.attributes.garnishes.forEach(garnish => {
+        const garnishLine = document.createElement('li.garnish');
+        garnishLine.innerHTML = `<li>${garnish.name}</li>`;
+        garnishesElement.appendChild(garnishLine);
+      });
       cardBackIngredients.appendChild(garnishesElement);
     }
 
@@ -172,54 +172,54 @@ class Study {
     cardBackIngredients.appendChild(cardBackIngredientsButtons);
 
     const cardBackFullRecipe = document.createElement('div');
-    cardBackFullRecipe.innerHTML = `<div.card-back-title><h2><b>${recipe.attributes.name}</b></h2></div>`
+    cardBackFullRecipe.innerHTML = `<div.card-back-title><h2><b>${recipe.attributes.name}</b></h2></div>`;
     cardBackFullRecipe.classList.add('card-back-full-recipe');
     if (recipe.attributes.liquors_array.length > 0) {
-      const liquorsTitle = document.createElement('h4.liquorsTitle')
-      liquorsTitle.innerHTML = '<b><u>Liquors</u></b>'
-      cardBackFullRecipe.appendChild(liquorsTitle)
-      const liquorsElement = document.createElement('ul.liquors')
-      const liquors = recipe.attributes.liquors_array.forEach( liquor =>
-        {const liquor_line = document.createElement('li.liquor')
-        liquor_line.innerHTML = `<li>${liquor}</li>`
-        liquorsElement.appendChild(liquor_line)
-      })
+      const liquorsTitle = document.createElement('h4.liquorsTitle');
+      liquorsTitle.innerHTML = '<b><u>Liquors</u></b>';
+      cardBackFullRecipe.appendChild(liquorsTitle);
+      const liquorsElement = document.createElement('ul.liquors');
+      const liquors = recipe.attributes.liquors_array.forEach(liquor => {
+        const liquorLine = document.createElement('li.liquor');
+        liquorLine.innerHTML = `<li>${liquor}</li>`;
+        liquorsElement.appendChild(liquorLine);
+      });
       cardBackFullRecipe.appendChild(liquorsElement);
     }
     if (recipe.attributes.liqueurs_array.length > 0) {
-      const liqueursTitle = document.createElement('h4.liqueursTitle')
-      liqueursTitle.innerHTML = '<b><u>Liqueurs</u></b>'
-      cardBackFullRecipe.appendChild(liqueursTitle)
-      const liqueursElement = document.createElement('ul.liqueurs')
-      const liqueurs = recipe.attributes.liqueurs_array.forEach( liqueur =>
-        {const liqueur_line = document.createElement('li.liqueur')
-        liqueur_line.innerHTML = `<li>${liqueur}</li>`
-        liqueursElement.appendChild(liqueur_line)
-      })
+      const liqueursTitle = document.createElement('h4.liqueursTitle');
+      liqueursTitle.innerHTML = '<b><u>Liqueurs</u></b>';
+      cardBackFullRecipe.appendChild(liqueursTitle);
+      const liqueursElement = document.createElement('ul.liqueurs');
+      const liqueurs = recipe.attributes.liqueurs_array.forEach(liqueur => {
+        const liqueurLine = document.createElement('li.liqueur');
+        liqueurLine.innerHTML = `<li>${liqueur}</li>`;
+        liqueursElement.appendChild(liqueurLine);
+      });
       cardBackFullRecipe.appendChild(liqueursElement);
     }
     if (recipe.attributes.mixers_array.length > 0) {
-      const mixersTitle = document.createElement('h4.mixersTitle')
-      mixersTitle.innerHTML = '<b><u>Mixers</u></b>'
-      cardBackFullRecipe.appendChild(mixersTitle)
-      const mixersElement = document.createElement('ul.mixers')
-      const mixers = recipe.attributes.mixers_array.forEach( mixer =>
-        {const mixer_line = document.createElement('li.mixer')
-        mixer_line.innerHTML = `<li>${mixer}</li>`
-        mixersElement.appendChild(mixer_line)
-      })
+      const mixersTitle = document.createElement('h4.mixersTitle');
+      mixersTitle.innerHTML = '<b><u>Mixers</u></b>';
+      cardBackFullRecipe.appendChild(mixersTitle);
+      const mixersElement = document.createElement('ul.mixers');
+      const mixers = recipe.attributes.mixers_array.forEach(mixer => {
+        const mixerLine = document.createElement('li.mixer');
+        mixerLine.innerHTML = `<li>${mixer}</li>`;
+        mixersElement.appendChild(mixerLine);
+      });
       cardBackFullRecipe.appendChild(mixersElement);
     }
     if (recipe.attributes.garnishes_array.length > 0) {
-      const garnishesTitle = document.createElement('h4.garnishesTitle')
-      garnishesTitle.innerHTML = '<b><u>Garnishes</u></b>'
-      cardBackFullRecipe.appendChild(garnishesTitle)
-      const garnishesElement = document.createElement('ul.garnishes')
-      const garnishes = recipe.attributes.garnishes_array.forEach( garnish =>
-        {const garnish_line = document.createElement('li.garnish')
-        garnish_line.innerHTML = `<li>${garnish}</li>`
-        garnishesElement.appendChild(garnish_line)
-      })
+      const garnishesTitle = document.createElement('h4.garnishesTitle');
+      garnishesTitle.innerHTML = '<b><u>Garnishes</u></b>';
+      cardBackFullRecipe.appendChild(garnishesTitle);
+      const garnishesElement = document.createElement('ul.garnishes');
+      const garnishes = recipe.attributes.garnishes_array.forEach(garnish => {
+        const garnishLine = document.createElement('li.garnish');
+        garnishLine.innerHTML = `<li>${garnish}</li>`;
+        garnishesElement.appendChild(garnishLine);
+      });
       cardBackFullRecipe.appendChild(garnishesElement);
     }
     const cardBackFullRecipeButtons = document.createElement('div');
@@ -235,28 +235,28 @@ class Study {
     cardBackFullRecipe.appendChild(cardBackFullRecipeButtons);
 
     card.appendChild(cardFront);
-    revealIngredientsButton.addEventListener('click', () => {this.revealIngredients(card, cardBackIngredients)})
-    backRevealIngredientsButton.addEventListener('click', () => {this.revealIngredients(card, cardBackIngredients)})
+    revealIngredientsButton.addEventListener('click', () => { this.revealIngredients(card, cardBackIngredients); });
+    backRevealIngredientsButton.addEventListener('click', () => { this.revealIngredients(card, cardBackIngredients); });
 
-    showRecipeNameButton.addEventListener('click', () => {this.showRecipeName(card, cardFront)})
-    secondShowRecipeNameButton.addEventListener('click', () => {this.showRecipeName(card, cardFront)})
+    showRecipeNameButton.addEventListener('click', () => { this.showRecipeName(card, cardFront); });
+    secondShowRecipeNameButton.addEventListener('click', () => { this.showRecipeName(card, cardFront); });
 
-    revealFullRecipeButton.addEventListener('click', () => {this.revealFullRecipe(card, cardBackFullRecipe)})
-    backRevealFullRecipeButton.addEventListener('click', () => {this.revealFullRecipe(card, cardBackFullRecipe)})
+    revealFullRecipeButton.addEventListener('click', () => { this.revealFullRecipe(card, cardBackFullRecipe); });
+    backRevealFullRecipeButton.addEventListener('click', () => { this.revealFullRecipe(card, cardBackFullRecipe); });
     return card;
   }
 
-  revealIngredients(card, cardBackIngredients) {
+  revealIngredients (card, cardBackIngredients) {
     card.innerHTML = '';
     card.appendChild(cardBackIngredients);
   }
 
-  revealFullRecipe(card, cardBackFullRecipe) {
+  revealFullRecipe (card, cardBackFullRecipe) {
     card.innerHTML = '';
     card.appendChild(cardBackFullRecipe);
   }
 
-  showRecipeName(card, cardFront) {
+  showRecipeName (card, cardFront) {
     card.innerHTML = '';
     card.appendChild(cardFront);
   }
@@ -264,4 +264,4 @@ class Study {
 
 const studySession = new Study();
 
-window.addEventListener('DOMContentLoaded', () => {studySession.render()});
+window.addEventListener('DOMContentLoaded', () => { studySession.render(); });

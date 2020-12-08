b837b561a69e01c0e7de278d46f8114f5b45b143
David <dmaster18@gmail.com>
2020-12-07 20:39:22 -0500

    Updated leaderboard.js

--- front_end/scripts/leaderboard.js
+++ front_end/scripts/leaderboard.js
@@ -21,7 +21,8 @@ class Leaderboard {
     const leaderboard = document.createElement('table');
     leaderboard.classList.add('leaderboard');
     const leaderboardHeader = '<tr><th>Name</th><th>Score</th><th>Percentage</th></tr><tr>';
-    const arrayOfUserData = userData.map(user => `<tr><td>${user.attributes.name}</td> <td>${user.attributes.score} Points</td> <td>${user.attributes.percentage}%</td></tr>`);
+    const sortedUserData = userData.sort(function(a, b) {return a.attributes.name - b.attributes.name})
+    const arrayOfUserData = sortedUserData.map(user => `<tr><td>${user.attributes.name}</td> <td>${user.attributes.score} Points</td> <td>${user.attributes.percentage}%</td></tr>`);
     const leaderboardData = arrayOfUserData.join(' ');
     leaderboard.innerHTML = `${leaderboardHeader} ${leaderboardData}`;
     main.appendChild(leaderboard);

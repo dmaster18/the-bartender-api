a7ad7d42b44ebae916d16272a8be91600c057964
David <50710431+dmaster18@users.noreply.github.com>
2020-12-08 14:09:13 -0500

    Implement a JS sort alphabetical sort function

--- front_end/scripts/leaderboard.js
+++ front_end/scripts/leaderboard.js
@@ -21,7 +21,7 @@ class Leaderboard {
     const leaderboard = document.createElement('table');
     leaderboard.classList.add('leaderboard');
     const leaderboardHeader = '<tr><th>Name</th><th>Score</th><th>Percentage</th></tr><tr>';
-    const sortedUserData = userData.sort(function (a, b) { return a.attributes.name.localeCompare(b.attributes.name); });
+    const sortedUserData = userData.sort(function (a, b) { return a.attributes.name.localeCompare(b.attributes.name); }); //Sorts leaderboard alphabetically using JS instead of Rails
     const arrayOfUserData = sortedUserData.map(user => `<tr><td>${user.attributes.name}</td> <td>${user.attributes.score} Points</td> <td>${user.attributes.percentage}%</td></tr>`);
     const leaderboardData = arrayOfUserData.join(' ');
     leaderboard.innerHTML = `${leaderboardHeader} ${leaderboardData}`;
